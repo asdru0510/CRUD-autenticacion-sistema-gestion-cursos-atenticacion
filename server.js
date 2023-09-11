ee8367037ec61783e649c631076958a1af0098ac
Asdrubal Sifontes <sifontesupta@gmail.com>
2023-09-11 01:01:35 -0300

    formatear server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express'); // Importamos express
-const userControllers = require('./controllers/user.controller'); //Importamos los controllers
-const bootcampControllers = require('./controllers/bootcamp.controller');//Importamos los controllers
+const express = require("express"); // Importamos express
+const userControllers = require("./controllers/user.controller"); //Importamos los controllers
+const bootcampControllers = require("./controllers/bootcamp.controller"); //Importamos los controllers
 
 const app = express(); //Envolvemos express en la variable 'app'
 
-require('dotenv').config({path:'./.env'})
+require("dotenv").config({ path: "./.env" });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 // Middleware para analizar datos en formato JSON
 app.use(express.json());
@@ -15,10 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //Uso de rutas
-app.use('/api',userControllers.router);
-app.use('/api',bootcampControllers.router);
+app.use("/api", userControllers.router);
+app.use("/api", bootcampControllers.router);
 
 //Corremos el servidor
-app.listen(PORT, 
-  () => {console.log(`Listening in the port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Listening in the port ${PORT}`);
 });
